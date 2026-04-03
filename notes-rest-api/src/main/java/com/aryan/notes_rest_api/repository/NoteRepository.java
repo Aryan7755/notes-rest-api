185a4e1880ee96819faf090e7652794c2aaa0757
Aryan raj <aryanbanthu@gmail.com>
2026-04-03 15:48:07 +0530

    Extended JPARepository in NoteRepo class.

--- notes-rest-api/src/main/java/com/aryan/notes_rest_api/repository/NoteRepository.java
+++ notes-rest-api/src/main/java/com/aryan/notes_rest_api/repository/NoteRepository.java
@@ -1,4 +1,10 @@
 package com.aryan.notes_rest_api.repository;
 
-public class NoteRepository {
+import com.aryan.notes_rest_api.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    // Standard CRUD methods are automatically included
 }
