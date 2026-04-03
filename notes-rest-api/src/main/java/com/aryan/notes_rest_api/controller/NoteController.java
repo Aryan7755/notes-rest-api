afb0d8575aac6d9c89cc4455acb1c35508bc035e
Aryan raj <aryanbanthu@gmail.com>
2026-04-03 15:55:39 +0530

    Added the HTTP request handling for crud in Note Controller class.

--- notes-rest-api/src/main/java/com/aryan/notes_rest_api/controller/NoteController.java
+++ notes-rest-api/src/main/java/com/aryan/notes_rest_api/controller/NoteController.java
@@ -1,4 +1,29 @@
 package com.aryan.notes_rest_api.controller;
 
+import com.aryan.notes_rest_api.model.Note;
+import com.aryan.notes_rest_api.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
 public class NoteController {
+    @Autowired
+    private NoteService service;
+
+    @GetMapping
+    public List<Note> getAll() { return service.getAllNotes(); }
+
+    @PostMapping
+    public Note create(@RequestBody Note note) { return service.createNote(note); }
+
+    @PutMapping("/{id}")
+    public Note update(@PathVariable Long id, @RequestBody Note note) {
+        return service.updateNote(id, note);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.deleteNote(id); }
 }
