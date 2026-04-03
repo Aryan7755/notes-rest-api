0307e07d0120608b47bf5a8a418757f2c702e23e
Aryan raj <aryanbanthu@gmail.com>
2026-04-03 15:39:44 +0530

    Initial commit - Spring Boot Notes API setup

---
+++ notes-rest-api/src/main/java/com/aryan/notes_rest_api/NotesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.notes_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesRestApiApplication.class, args);
+	}
+
+}
